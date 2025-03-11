cc1cf7dd155280f2969243c452bd2c915135701b
ev3nst <futuregnr@gmail.com>
2025-03-11 03:00:52 +0300

    no need for confirmation when closing app

--- src/header/window-actions.tsx
+++ src/header/window-actions.tsx
@@ -2,17 +2,6 @@ import { XIcon, SquareIcon, MinusIcon } from 'lucide-react';
 
 import { getCurrentWindow } from '@tauri-apps/api/window';
 
-import {
-	AlertDialog,
-	AlertDialogAction,
-	AlertDialogCancel,
-	AlertDialogContent,
-	AlertDialogDescription,
-	AlertDialogFooter,
-	AlertDialogHeader,
-	AlertDialogTitle,
-	AlertDialogTrigger,
-} from '@/components/alert-dialog';
 import { Button } from '@/components/button';
 
 import { cn } from '@/lib/utils';
@@ -48,31 +37,13 @@ export function WindowActions({ className }: { className?: string }) {
 			>
 				<SquareIcon className="h-[0.85rem] w-[0.85rem]" />
 			</Button>
-			<AlertDialog>
-				<AlertDialogTrigger asChild>
-					<Button
-						variant="ghost"
-						className="clickable-content h-8 w-8 px-0 hover:bg-red-800"
-					>
-						<XIcon className="h-[1.2rem] w-[1.2rem]" />
-					</Button>
-				</AlertDialogTrigger>
-				<AlertDialogContent>
-					<AlertDialogHeader>
-						<AlertDialogTitle>Close Application</AlertDialogTitle>
-						<AlertDialogDescription>
-							If there is any active process running they might
-							not close along with the application.
-						</AlertDialogDescription>
-					</AlertDialogHeader>
-					<AlertDialogFooter>
-						<AlertDialogCancel>Cancel</AlertDialogCancel>
-						<AlertDialogAction onClick={handleClose}>
-							Continue
-						</AlertDialogAction>
-					</AlertDialogFooter>
-				</AlertDialogContent>
-			</AlertDialog>
+			<Button
+				variant="ghost"
+				className="clickable-content h-8 w-8 px-0 hover:bg-red-800"
+				onClick={handleClose}
+			>
+				<XIcon className="h-[1.2rem] w-[1.2rem]" />
+			</Button>
 		</div>
 	);
 }
