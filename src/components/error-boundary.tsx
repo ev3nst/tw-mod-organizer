c902eac29545a9e47e8f1df8912205138b8a7c62
ev3nst <futuregnr@gmail.com>
2025-03-11 03:03:06 +0300

    error boundary has window actions now.

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from '@/components/button';
+import { WindowActions } from '@/header/window-actions';
 
 // Define the props type
 interface ErrorBoundaryProps {
@@ -32,29 +33,40 @@ export class ErrorBoundary extends React.Component<
 	render(): React.ReactNode {
 		if (this.state.hasError) {
 			return (
-				<div className="flex items-center justify-center h-screen w-screen app-drag-region">
-					<div className="relative text-center ">
-						<h1 className="text-balance text-5xl font-semibold tracking-tight text-primary sm:text-7xl">
-							Error
-						</h1>
-						<p className="mt-6 text-pretty text-lg font-medium text-muted-foreground sm:text-xl/8">
-							Unexpected error has occured.
-						</p>
+				<main>
+					<div className="flex justify-between app-drag-region border-b fixed top-0 left-0 right-0 py-1">
+						<div className="flex items-center gap-4 py-1 px-3">
+							<img src="/logo.png" className="h-6 w-6" />
+							<div className="text-sm font-bold">
+								TW Mod Organizer
+							</div>
+						</div>
+						<WindowActions className="px-1" />
+					</div>
+					<div className="flex items-center justify-center h-screen w-screen app-drag-region">
+						<div className="relative text-center ">
+							<h1 className="text-balance text-5xl font-semibold tracking-tight text-primary sm:text-7xl">
+								Error
+							</h1>
+							<p className="mt-6 text-pretty text-lg font-medium text-muted-foreground sm:text-xl/8">
+								Unexpected error has occured.
+							</p>
 
-						<p className="mt-2 text-pretty text-md font-medium text-muted-foreground">
-							You may check console for further details.
-						</p>
-						<div className="mt-10 flex flex-col sm:flex-row sm:items-center sm:justify-center gap-y-3 gap-x-6 clickable-content">
-							<Button
-								onClick={() => {
-									window.location.reload();
-								}}
-							>
-								Reload
-							</Button>
+							<p className="mt-2 text-pretty text-md font-medium text-muted-foreground">
+								You may check console for further details.
+							</p>
+							<div className="mt-10 flex flex-col sm:flex-row sm:items-center sm:justify-center gap-y-3 gap-x-6 clickable-content">
+								<Button
+									onClick={() => {
+										window.location.reload();
+									}}
+								>
+									Reload
+								</Button>
+							</div>
 						</div>
 					</div>
-				</div>
+				</main>
 			);
 		}
 
