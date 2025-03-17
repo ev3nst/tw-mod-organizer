8a8af2d13ebd950d6b0905ef113da72e8cf16f88
ev3nst <futuregnr@gmail.com>
2025-03-17 18:25:02 +0300

    switched to using open crate for external links

--- src-tauri/src/utils/open_external_url.rs
+++ src-tauri/src/utils/open_external_url.rs
@@ -1,16 +1,10 @@
-use std::{os::windows::process::CommandExt, process::Command};
+use open;
 
 #[tauri::command(rename_all = "snake_case")]
 pub fn open_external_url(url: String) -> Result<(), String> {
     if !(url.starts_with("http://") || url.starts_with("https://") || url.starts_with("steam://")) {
-        return Err("Invalid URL: must start with http:// or https:// or steam://".to_string());
+        return Err("Invalid URL: must start with http://, https://, or steam://".to_string());
     }
 
-    Command::new("cmd")
-        .creation_flags(0x08000000)
-        .args(&["/C", "start", &url])
-        .spawn()
-        .map_err(|e| format!("Failed to open URL: {}", e))?;
-
-    Ok(())
+    open::that(url).map_err(|e| format!("Failed to open URL: {}", e))
 }
