c561f088e73afa4c05f4d82d652cfd09c9cbed6a
ev3nst <futuregnr@gmail.com>
2025-03-20 21:49:55 +0300

    added information regarding save game mod compatibility feature

--- src/sidebar/index.tsx
+++ src/sidebar/index.tsx
@@ -7,10 +7,20 @@ import {
 	AccordionItem,
 	AccordionTrigger,
 } from '@/components/accordion';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/dialog';
 
 import { Downloads } from './downloads';
 import { Saves } from './saves';
 import { Play } from './play';
+import { Button } from '@/components/button';
+import { InfoIcon } from 'lucide-react';
 
 export function AppSidebar() {
 	const [tab, setActiveTab] = useState<string>('saves');
@@ -30,9 +40,51 @@ export function AppSidebar() {
 						className="flex flex-col"
 					>
 						<AccordionItem value="saves">
-							<AccordionTrigger className="text-base font-medium text-foreground px-3 py-2 sticky top-0 bg-background z-10">
-								Saves
-							</AccordionTrigger>
+							<div className="flex justify-between items-center">
+								<AccordionTrigger className="text-base font-medium text-foreground px-3 py-2 sticky top-0 bg-background z-10 w-[300px]">
+									Saves
+								</AccordionTrigger>
+								<Dialog>
+									<DialogTrigger asChild>
+										<Button
+											variant="ghost"
+											size="icon"
+											className="text-blue-500 m-0 flex-shrink"
+											onClick={e => e.stopPropagation()}
+										>
+											<InfoIcon />
+										</Button>
+									</DialogTrigger>
+									<DialogContent>
+										<DialogHeader>
+											<DialogTitle>
+												Save Game Mod Compatibility
+											</DialogTitle>
+											<DialogDescription asChild>
+												<div className="!text-base pt-2">
+													Mod manager in the event of
+													a save game file being
+													created or modified while
+													game is running, stores
+													information for that save
+													game. This information is
+													later used if that save game
+													is compatible with mods in
+													your profile and informs you
+													accordingly. If there is a
+													save game file that is
+													created while mod manager
+													was not open they will be
+													excempt from this process as
+													there is no way to know
+													which mods were active at
+													that time.
+												</div>
+											</DialogDescription>
+										</DialogHeader>
+									</DialogContent>
+								</Dialog>
+							</div>
 							<AccordionContent className="overflow-auto max-h-[calc(100vh-12rem)]">
 								<Saves />
 							</AccordionContent>
