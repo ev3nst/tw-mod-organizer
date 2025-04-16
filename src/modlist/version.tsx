2aea689288e651f1a24e7ff1737d593d63097e24
ev3nst <futuregnr@gmail.com>
2025-04-16 18:21:13 +0300

    mod version check bugfix

--- src/modlist/version.tsx
+++ src/modlist/version.tsx
@@ -6,7 +6,7 @@ import { TableHead } from '@/components/table';
 import api from '@/lib/api';
 import { settingStore } from '@/lib/store/setting';
 import { ModItem, modsStore } from '@/lib/store/mods';
-import { modVersionStore } from '@/lib/store/mod_version';
+import { ModVersionItem, modVersionStore } from '@/lib/store/mod_version';
 
 export const THVersion = memo(() => {
 	const selectedGame = settingStore(state => state.selectedGame);
@@ -24,85 +24,74 @@ export const THVersion = memo(() => {
 
 	useEffect(() => {
 		if (modVersionData.length > 0) {
-			(async () => {
-				const ONE_WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
-				const now = Date.now();
-				let updatedModVersionData = [...modVersionData];
-				let changed = [];
-				for (let vi = 0; vi < modVersionData.length; vi++) {
-					const mv = modVersionData[vi];
-					switch (mv.mod_type) {
-						case 'steam_mod':
-							if (mv.version !== mv.latest_version) {
+			const ONE_WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
+			const now = Date.now();
+			let updatedModVersionData = [...modVersionData];
+			let changed = [];
+
+			const fetchModDetails = async (mv: ModVersionItem, vi: number) => {
+				const modDetails = mods.find(
+					m => m.identifier === mv.mod_id,
+				) as ModItem;
+				if (
+					modDetails &&
+					typeof modDetails.url === 'string' &&
+					modDetails.url.length > 0 &&
+					modDetails.url.startsWith('https://www.nexusmods.com')
+				) {
+					const modNexusId = modDetails.url.split('/').pop();
+					if (modNexusId) {
+						try {
+							const nexusModDetail = await api.nexus_mod_details({
+								game_domain_name: selectedGame!.nexus_slug,
+								mod_id: Number(modNexusId),
+							});
+							console.log(nexusModDetail, 'nexusModDetail');
+							updatedModVersionData[vi].last_time_checked =
+								Date.now();
+							if (mv.latest_version !== nexusModDetail.version) {
+								updatedModVersionData[vi].latest_version =
+									nexusModDetail.version;
+								mv.latest_version = nexusModDetail.version;
 								changed.push(mv);
 							}
-							break;
-						case 'local_mod':
-							if (mv.version !== mv.latest_version) {
-								changed.push(mv);
-							}
-							break;
-						case 'nexus_mod':
-							const timeSinceCheck = now - mv.last_time_checked;
-							if (timeSinceCheck > ONE_WEEK_IN_MS) {
-								try {
-									const modDetails = mods.find(
-										m => m.identifier === mv.mod_id,
-									) as ModItem;
-									if (
-										modDetails &&
-										typeof modDetails.url === 'string' &&
-										modDetails.url.length > 0 &&
-										modDetails.url.startsWith(
-											'https://www.nexusmods.com',
-										)
-									) {
-										const modNexusId = modDetails.url
-											.split('/')
-											.pop();
-										if (
-											typeof modNexusId === 'string' &&
-											modNexusId.length > 0
-										) {
-											const nexusModDetail =
-												await api.nexus_mod_details({
-													game_domain_name:
-														selectedGame!
-															.nexus_slug,
-													mod_id: Number(modNexusId),
-												});
-											updatedModVersionData[
-												vi
-											].last_time_checked = Date.now();
-											if (
-												mv.latest_version !==
-												nexusModDetail.version
-											) {
-												updatedModVersionData[
-													vi
-												].latest_version =
-													nexusModDetail.version;
-												mv.latest_version =
-													nexusModDetail.version;
-												changed.push(mv);
-											}
-										}
-									}
-								} catch (_e) {}
-							} else if (mv.latest_version !== mv.version) {
-								changed.push(mv);
-							}
-							break;
-						default:
-							break;
+						} catch (_e) {}
 					}
 				}
+			};
 
+			let updated = false;
+			for (let vi = 0; vi < modVersionData.length; vi++) {
+				const mv = modVersionData[vi];
+				switch (mv.mod_type) {
+					case 'steam_mod':
+					case 'local_mod':
+						if (mv.version !== mv.latest_version) {
+							changed.push(mv);
+						}
+						break;
+					case 'nexus_mod':
+						const timeSinceCheck = now - mv.last_time_checked;
+						if (timeSinceCheck > ONE_WEEK_IN_MS) {
+							// Only check if more than 1 week has passed since the last check
+							fetchModDetails(mv, vi);
+							updated = true;
+						} else if (mv.latest_version !== mv.version) {
+							changed.push(mv);
+						}
+						break;
+					default:
+						break;
+				}
+			}
+
+			if (updated) {
 				setVersionData(updatedModVersionData);
-				setChangedMods(changed);
-			})();
+			}
+
+			setChangedMods(changed);
 		}
-	}, []);
+	}, [modVersionData, selectedGame!.steam_id]);
 
 	if (!toggle_version) return null;
 	return (
